dff9284257157091485161e5ba647d93f3ca72bd
frog-fletcher <karolism207@gmail.com>
2022-07-01 14:26:12 +0100

    2nd page wip

--- main.go
+++ main.go
@@ -220,15 +220,15 @@ func CheckErr(e error) {
 	fmt.Println(e)
 }
 func main() {
-	
+
 	db, err := sql.Open("sqlite3", "./database/champion.db")
 	if err != nil {
 		CheckErr(err)
 	}
-	
+
 	data := database.Connect(db)
 	database.Connect(db)
-	
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Handler)
 	// mux.HandleFunc("/style", css)
@@ -279,7 +279,7 @@ func main() {
 			StrongVS: strong,
 			WeakVS:   weak,
 		})
-		
+
 	}
 	fmt.Println("Starting server at port 8080:\n http://localhost:8080/home")
 
@@ -288,6 +288,7 @@ func main() {
 		fmt.Printf("main ListenAndServe error: %+v\n", err)
 	}
 }
+
 //joins string
 func champ(s []string) string {
 	return strings.Join(s, "\n")
@@ -295,7 +296,7 @@ func champ(s []string) string {
 
 //handles home page
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-		db, err := sql.Open("sqlite3", "./database/champion.db")
+	db, err := sql.Open("sqlite3", "./database/champion.db")
 	if err != nil {
 		CheckErr(err)
 	}
@@ -306,6 +307,26 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tpl := template.Must(template.ParseGlob("templates/*"))
 	tpl.ExecuteTemplate(w, "home.html", data.GetChamp())
 }
+func championHandler(w http.ResponseWriter, r *http.Request, name string) {
+	db, err := sql.Open("sqlite3", "./database/champion.db")
+	if err != nil {
+		CheckErr(err)
+	}
+	path := strings.Trim(name, "/home/")
+
+	data := database.Connect(db)
+	database.Connect(db)
+
+
+	for _, champion := range data.GetChamp(){
+		if champion.Name == path{
+			
+			tpl := template.Must(template.ParseGlob("templates/*"))
+			tpl.ExecuteTemplate(w, "champion.html", champion)
+		}
+	}
+
+}
 
 //handlers handles all the website endpoints
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -313,8 +334,12 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	pathLink := "./images"
 	pathLink += r.URL.Path
 	//name is used in order not to manually have to write the endpoint--- instead parsing it directly from url path as it will refer to an image to be served
-	name:= r.URL.Path
+	name := r.URL.Path
 	pathLink += ".jpg"
+
+
+	// champPath:= r.URL.Path
+	// fmt.Println(champPath)
 
 	switch r.URL.Path {
 	case "/style":
@@ -322,6 +347,10 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	case "/home":
 		homeHandler(w, r)
 	case name:
-		http.ServeFile(w,r, pathLink)
+		if strings.Contains(name, "/home/"){
+			championHandler(w,r, name)
+		} else{
+			http.ServeFile(w, r, pathLink)
+		}
 	}
 }
